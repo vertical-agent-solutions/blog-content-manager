74af59836299b15c13459e8515cc28749796d6f1
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2024-12-25 20:00:00 -0500

    Implement text stream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+
 from dotenv import load_dotenv
 import google.generativeai as genai
 import os
@@ -6,6 +7,9 @@ import os
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash-thinking-exp-1219")
 response = model.generate_content("Explain how AI works")
-print(response.text)
+for chunk in response:
+    print(chunk.text)
+    print("_" * 80)
+
