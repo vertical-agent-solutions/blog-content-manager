1dbb497e3e0c1d6b9e173f31251a926942516c2f
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2024-12-27 23:43:34 -0500

    Clean up

--- blog_generator/settings.py
+++ blog_generator/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-here'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
